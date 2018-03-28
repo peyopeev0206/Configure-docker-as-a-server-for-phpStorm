83099b3750af581de2db35dc0f62455c9358e5cb
Peyo Peev <peyopeev0206@gmail.com>
2018-03-28 22:31:50 +0300

    Add and mysql test

--- test-folder/index.php
+++ test-folder/index.php
@@ -6,3 +6,18 @@ $numberTwo = 2;
 $sumOfTwoNumbers = $numberOne + $numberTwo;
 
 echo ($sumOfTwoNumbers === 3) ? '1 plus 2 equals 3!' : 'Something is terribly wrong';
+echo '<br>';
+$servername = "docker-mysql";
+$username = "root";
+$password = "docker";
+$database = "blog";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
